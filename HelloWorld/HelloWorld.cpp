09c9f27e8cf71c781121ebe822024e31a3948dd6
Michael Richards <michael.richards23@gmail.com>
2020-05-23 18:06:43 +0100

    Later Change Merged

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
    //This has no documentation but it doesn't need it
-    vector<string> msg {"Hello MR 123", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Hello MR 123", "C++", "World", "from", "VS Code", "and the C++ extension!", "and the Github experience"};
 
     for (const string& word : msg)
     {

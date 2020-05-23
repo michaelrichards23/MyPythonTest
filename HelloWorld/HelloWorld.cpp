19954ed4d4611bbbd9a17c10e824f3e521fbaef3
Michael Richards <39367581+michaelrichards23@users.noreply.github.com>
2020-05-23 17:58:48 +0100

    Added comments

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+   //This has no documentation but it doesn't need it
     vector<string> msg {"Hello MR 123", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
     for (const string& word : msg)
